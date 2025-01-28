1a4b23240975592f104e028066e18b59e04e16d3
Hector M. R. <3621110+hectr@users.noreply.github.com>
2025-01-28 18:20:00 +0100

    chore(deps): add SnapshotTesting dependency to package

--- Package.swift
+++ Package.swift
@@ -10,7 +10,9 @@ let package = Package(
     products: [
         .library(name: "LeanRedux", targets: ["LeanRedux"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.6"),
+    ],
     targets: [
         .target(
             name: "LeanRedux",
