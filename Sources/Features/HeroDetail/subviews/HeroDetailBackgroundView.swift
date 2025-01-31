f46d0ec8aa514e257e2090f16b2424bf1955af83
Hector M. R. <3621110+hectr@users.noreply.github.com>
2025-01-31 22:43:21 +0100

    refactor: rename View.State to View.ViewState
    - Includes minor code style changes.
    - Removes unneeded #Preview

--- Sources/Features/HeroDetail/subviews/HeroDetailBackgroundView.swift
+++ Sources/Features/HeroDetail/subviews/HeroDetailBackgroundView.swift
@@ -2,14 +2,20 @@ import SwiftUI
 
 struct HeroDetailBackgroundView: View
 {
+    // MARK: Depenedencies
+
     let from: Color
     let to: Color
+
+    // MARK: Lifecycle
 
     init(from: Color = .gray, to: Color = .black)
     {
         self.from = from
         self.to = to
     }
+
+    // MARK: Content
 
     var body: some View
     {
